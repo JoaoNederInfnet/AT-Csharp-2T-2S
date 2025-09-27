fc90a0a50ac9f2ecb86ebc284a22f4fe820e7ac3
João  Pedro Neder Resende de Lima <Jpnereli@Mac-mini.local>
2025-09-27 12:40:50 -0300

    0.0: Editando o gitignore

---
+++ AT-Csharp-2T-2S/AT-Csharp-2T-2S/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AT_Csharp_2T_2S.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
